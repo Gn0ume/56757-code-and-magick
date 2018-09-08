67ec0e700f53726c0ad17e5a215e8e2df9b8afa6
KravchukKseniya <dorovikova_kseniya@mail.ru>
2018-09-08 15:21:15 +0300

    Error fixed

--- js/stat.js
+++ js/stat.js
@@ -2,15 +2,15 @@
 
 var CLOUD = {
   height: 270,
-  weidht: 420,
+  width: 420,
   coordX: 100,
   coordY: 10,
   shadowShift: 10
 };
 
 var BAR = {
-  heigth: 150,
-  weidth: 40,
+  height: 150,
+  width: 40,
   paddingTop: 90
 };
 
@@ -25,7 +25,7 @@ var TEXT_PADDINGS = {
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD.weidht, CLOUD.height);
+  ctx.fillRect(x, y, CLOUD.width, CLOUD.height);
 };
 
 var getMaxElement = function (arr) {
@@ -39,26 +39,28 @@ var getMaxElement = function (arr) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD.coord_x + CLOUD.shadow_shift, CLOUD.coord_y + CLOUD.shadow_shift, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD.coord_x, CLOUD.coord_y, '#fff');
+  renderCloud(ctx, CLOUD.coordX + CLOUD.shadowShift, CLOUD.coordY + CLOUD.shadowShift, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD.coordX, CLOUD.coordY, '#fff');
 
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#000';
-  ctx.fillText('Ура! Вы победили!', TEXT_PADDINGS.left, TEXT_PADDINGS.main_line_1);
-  ctx.fillText('Список результатов:', TEXT_PADDINGS.left, TEXT_PADDINGS.main_line_2);
+  ctx.fillText('Ура! Вы победили!', TEXT_PADDINGS.left, TEXT_PADDINGS.mainLine1);
+  ctx.fillText('Список результатов:', TEXT_PADDINGS.left, TEXT_PADDINGS.mainLine2);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
+    var step = TEXT_PADDINGS.left + TEXT_PADDINGS.barStep * i;
+    var indent = BAR.height * (1 - times[i] / maxTime);
     ctx.fillStyle = '#000';
-    ctx.fillText(names[i], TEXT_PADDINGS.left + TEXT_PADDINGS.bar_step * i, TEXT_PADDINGS.textbar_top);
-    ctx.fillText(Math.round(times[i]), TEXT_PADDINGS.left + TEXT_PADDINGS.bar_step * i, TEXT_PADDINGS.scorebar_top + BAR.heigth * (1 - times[i] / maxTime));
+    ctx.fillText(names[i], step, TEXT_PADDINGS.textbarTop);
+    ctx.fillText(Math.round(times[i]), step, TEXT_PADDINGS.scorebarTop + indent);
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(TEXT_PADDINGS.left + TEXT_PADDINGS.bar_step * i, BAR.padding_top + BAR.heigth * (1 - times[i] / maxTime), BAR.weidth, (times[i] * BAR.heigth) / maxTime);
+      ctx.fillRect(step, BAR.paddingTop + indent, BAR.width, (times[i] * BAR.height) / maxTime);
     } else {
       ctx.fillStyle = 'rgba(14, 18, 207, ' + Math.random() + ')';
-      ctx.fillRect(TEXT_PADDINGS.left + TEXT_PADDINGS.bar_step * i, BAR.padding_top + BAR.heigth * (1 - times[i] / maxTime), BAR.weidth, (times[i] * BAR.heigth) / maxTime);
+      ctx.fillRect(step, BAR.paddingTop + indent, BAR.width, (times[i] * BAR.height) / maxTime);
     }
   }
 };
